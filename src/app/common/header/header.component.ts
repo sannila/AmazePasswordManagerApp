b168de6f56f683195e500db69b52907b13020349
Sanjai <sanjai@wildrepublictech.com>
2025-05-16 12:27:04 +0530

    header component updated

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,19 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { HttpSerivceService } from '../../services/http-serivce.service';
-import { Router } from '@angular/router';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
+import { Router, RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
+import { Menu } from 'primeng/menu';
 
 @Component({
   selector: 'app-header',
-  imports: [MatMenuModule, MatButtonModule],
+  imports: [ButtonModule, MenuModule, Menu, RouterModule],
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent implements OnInit {
-
   @Input() isLoggedIn: boolean = false;
+
+  menuItems: MenuItem[] | undefined;
 
   constructor(private httpService: HttpSerivceService, private router: Router) {
     if (!this.httpService.userValue) {
@@ -21,12 +24,25 @@ export class HeaderComponent implements OnInit {
       this.router.navigate(['/home']);
       return;
     }
-
-    
   }
 
   ngOnInit(): void {
-
+    this.menuItems = [
+      {
+                items: [
+                    {
+                        label: 'Profile',
+                        icon: 'pi pi-user-edit',
+                        command: () => this.router.navigate(['/dashboard/profile'])
+                    },
+                    {
+                        label: 'Sign Out',
+                        icon: 'pi pi-sign-out',
+                        command: () => this.signOut()
+                    }
+                ]
+      },
+    ];
   }
 
   signOut() {
